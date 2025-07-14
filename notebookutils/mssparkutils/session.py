a16eedb533bf0eb1dfe86c77bd414681b693342f
Federico <47462792+wtfzambo@users.noreply.github.com>
2025-07-14 17:46:06 +0200

    feat: add session

--- notebookutils/mssparkutils/session.py
+++ notebookutils/mssparkutils/session.py
@@ -1,6 +1,13 @@
+from warnings import deprecated
+
+__DEPRECATED_MSG = """The namespace mssparkutils is deprecated, please use notebookutils.session instead."""
+
+
+@deprecated(__DEPRECATED_MSG)
 def stop():
     pass
 
 
+@deprecated(__DEPRECATED_MSG)
 def restartPython():
     pass
--- notebookutils/session.py
+++ notebookutils/session.py
@@ -1,4 +1,14 @@
-def stop(detach=True):
+"""
+notebookutils.session utility supports you stop the current session.
+
+Below are the available methods in Notebook runtime utility:
+
+notebookutils.session.stop(detach: Boolean = true): Stop the current session.
+notebookutils.session.restartPython(): Restart the Python interpreter.
+"""
+
+
+def stop(detach: bool = True):
     pass
 
 
@@ -6,5 +16,5 @@ def restartPython():
     pass
 
 
-def help(method_name=None):
+def help(method_name: str | None = None):
     pass
