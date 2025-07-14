750b19bc90859c7b9060a060a6e47058bc1584fb
Federico <47462792+wtfzambo@users.noreply.github.com>
2025-07-14 17:14:20 +0200

    feat: add old credentials docstring

--- notebookutils/mssparkutils/credentials.py
+++ notebookutils/mssparkutils/credentials.py
@@ -1,3 +1,19 @@
+"""
+getToken(audience, name): returns AAD token for a given audience, name (optional)
+isValidToken(token): returns true if token hasn't expired
+getConnectionStringOrCreds(linkedService): returns connection string or credentials for linked service
+getFullConnectionString(linkedService): returns full connection string with credentials
+getPropertiesAll(linkedService): returns all the properties of a linked servicegetSPTokenWithCert(resource, authority, clientId, akvNameOrUri, certificateName): returns an access token for a service principal using a certificate stored in a AKV as the credential
+getSPTokenWithCertLS(resource, authority, clientId, akvLinkedService, certificateName): returns an access token for a service principal using a certificate stored in a AKV linked service as the credential
+getSecret(akvName, secret, linkedService): returns AKV secret for a given AKV linked service, akvName, secret key
+getSecret(akvName, secret): returns AKV secret for a given akvName, secret key
+getSecretWithLS(linkedService, secret): returns AKV secret for a given linked service, secret key
+putSecret(akvName, secretName, secretValue, linkedService): puts AKV secret for a given akvName, secretName
+putSecret(akvName, secretName, secretValue): puts AKV secret for a given akvName, secretName
+putSecretWithLS(linkedService, secretName, secretValue): puts AKV secret for a given linked service, secretName
+"""
+
+
 def getToken(audience, name=""):
     return ""
 
