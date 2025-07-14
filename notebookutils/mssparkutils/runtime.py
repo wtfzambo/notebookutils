474007ffc0627a124e8837b0b7f5d82511896cf8
Federico <47462792+wtfzambo@users.noreply.github.com>
2025-07-14 17:43:32 +0200

    feat: add runtime

--- notebookutils/mssparkutils/runtime.py
+++ notebookutils/mssparkutils/runtime.py
@@ -1,3 +1,12 @@
+"""
+mssparkutils.runtime utility supports you get context information of runtime that matters to current session.
+
+Below are the available methods in Notebook runtime utility:
+
+notebookutils.runtime.context: Map[String, Any] -> Get the current runtime context.
+notebookutils.runtime.help("methodName"): Show more details of a specific method.
+"""
+
 context = {}
 
 
