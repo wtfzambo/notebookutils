186d90eb3a1de7c876f524d041c6eb169a868873
Federico <47462792+wtfzambo@users.noreply.github.com>
2025-07-14 15:01:27 +0200

    deprecate old notebook module

--- notebookutils/mssparkutils/notebook.py
+++ notebookutils/mssparkutils/notebook.py
@@ -1,22 +1,33 @@
+from warnings import deprecated
+
+__DEPRECATED_MSG = """The namespace mssparkutils is deprecated, please use notebookutils.notebook instead."""
+
+
+@deprecated(__DEPRECATED_MSG)
 def updateNBSEndpoint(endpoint):
     pass
 
 
+@deprecated(__DEPRECATED_MSG)
 def help(method_name=None):
     pass
 
 
+@deprecated(__DEPRECATED_MSG)
 def run(path, timeout_seconds=90, arguments={}, workspace=""):
     return ""
 
 
+@deprecated(__DEPRECATED_MSG)
 def exit(value):
     pass
 
 
+@deprecated(__DEPRECATED_MSG)
 def runMultiple(dag, config=None):
     pass
 
 
+@deprecated(__DEPRECATED_MSG)
 def validateDAG(dag):
     pass
